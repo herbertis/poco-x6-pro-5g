01e412526266f82e73d738dea265a73a66227161
Herbert Ishimura Sousa <herbert_is@hotmail.com>
2024-08-19 12:38:15 -0300

    vs01

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.galeria').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  lazyLoad: 'ondemand', controls: false,
+   
+});
+
+$('.multiple-items').slick({
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 0,
+  speed: 9000,
+  variableWidth: true, 
+  centerMode: false,
+  cssEase: "linear",
+ 
+});
+
+		
